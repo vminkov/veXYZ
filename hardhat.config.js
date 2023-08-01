564786dfe4048761d1b0228006f030b638e34e9e
qzirak <xirexor.development@gmail.com>
2023-08-01 19:03:44 +0500

    feature_major_major/voteEscrow

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-truffle5');
+require('@nomiclabs/hardhat-web3');
+require("hardhat-gas-reporter");
+require('hardhat-contract-sizer');
+require("solidity-coverage");
+require('hardhat-spdx-license-identifier');
+require('hardhat-abi-exporter');
+require('hardhat-storage-layout');
+require('@openzeppelin/hardhat-upgrades');
+const fs = require("fs");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.13",
+};
