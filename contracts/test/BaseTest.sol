bb243728ac3a863448a25d2d7e8634f8cd149a81
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-08-11 11:02:22 +0300

    testing the locking and voting power

--- contracts/test/BaseTest.sol
+++ contracts/test/BaseTest.sol
@@ -14,17 +14,24 @@ import { VoteEscrow } from "../VoteEscrow.sol";
 
 contract IonicToken is ERC20 {
   constructor() ERC20("IONIC", "ION", 18) {}
+
+  function mint(address to, uint256 amount) public {
+    _mint(to, amount);
+  }
 }
 
 contract BaseTest is Test {
   GaugeFactory public gaugeFactory;
   Voter public voter;
   VoteEscrow public ve;
+  IonicToken ionicToken = new IonicToken();
+  address proxyAdmin = address(123);
+  address bridge1 = address(321);
 
   function setUp() public {
-    IonicToken ionicToken = new IonicToken();
+    vm.chainId(ve.ARBITRUM_ONE());
 
-    address proxyAdmin = address(123);
+    ionicToken = new IonicToken();
 
     VoterRolesAuthority voterRolesAuthImpl = new VoterRolesAuthority();
     TransparentUpgradeableProxy rolesAuthProxy = new TransparentUpgradeableProxy(
@@ -53,9 +60,32 @@ contract BaseTest is Test {
     TransparentUpgradeableProxy voterProxy = new TransparentUpgradeableProxy(address(voterImpl), proxyAdmin, "");
     voter = Voter(address(voterProxy));
     voter.initialize(address(ve), address(gaugeFactory), address(bribeFactory), voterRolesAuth);
+
+    vm.prank(ve.owner());
+    ve.addBridge(bridge1);
   }
 
   function testOwner() public {
     assertEq(gaugeFactory.owner(), address(this), "!owner");
+  }
+
+  function testIonicLockAndVotingPower() public {
+    uint256 tokenId;
+
+    ionicToken.mint(address(this), 100e18);
+
+    ionicToken.approve(address(ve), 1e36);
+
+    // change to some other chain ID
+    vm.chainId(1);
+
+    vm.expectRevert("wrong chain id");
+    tokenId = ve.create_lock(20e18, 52 weeks);
+
+    // revert back to the master chain ID
+    vm.chainId(ve.ARBITRUM_ONE());
+    tokenId = ve.create_lock(20e18, 52 weeks);
+
+    assertApproxEqAbs(ve.balanceOfNFT(tokenId), 20e18, 1e17, "wrong voting power");
   }
 }
