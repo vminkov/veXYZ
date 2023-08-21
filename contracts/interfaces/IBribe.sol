6811157470978ec064c1ccb15de4eb42c7b2ef37
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-08-21 09:46:05 +0300

    ionic token; upgrade to solidity 0.8.19

--- contracts/interfaces/IBribe.sol
+++ contracts/interfaces/IBribe.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.19;
 
 interface IBribe {
   function getRewardForOwner(uint tokenId, address[] memory tokens) external;
